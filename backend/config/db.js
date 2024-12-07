fc645071d2718f9026286e0e4a66c8ad328a6bd8
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2024-12-07 11:26:50 +0530

    prod1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
  const mongoose = require('mongoose');
 
 exports.connect = () => {
-    mongoose.connect(`${process.env.DB_URI}/${process.env.DB_NAME}`, {})
+    mongoose.connect(`${process.env.DB_URI}`, {
+        dbName: "EventManager",
+    })
         .then(() => console.log('DB Connected'))
         .catch((err) => console.log(err));
 }
